bdce4b14c6d6c57f05c3730101afc17122d7cbd6
jay415404 <jay416505@gmail.com>
2021-12-04 17:18:38 +0530

    chor: added s3-deploy to github actions

---
+++ src/common/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+                                              
+const baseURL = "http://127.0.0.1:80"
+// const baseURL = "server-url"
+
+export const axiosInstance = axios.create({
+  baseURL: baseURL,
+  headers: {
+    'Content-Type': 'application/json',
+    "Access-Control-Allow-Origin" : "*",
+  }
+});
