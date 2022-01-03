d65473403188ddd2e500f51f26cac2cdcf422d94
jay415404 <jay416505@gmail.com>
2022-01-03 08:58:22 +0530

    chor: updated node url

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
-const baseURL = "http://127.0.0.1:5000"
+// const baseURL = "http://127.0.0.1:5000"
 // const baseURL = "server-url"
-// const baseURL = "https://pesto-mentee-tracker.herokuapp.com"
+const baseURL = "https://pesto-mentee-tracker.herokuapp.com";
 
 const axiosInstance = axios.create({
   baseURL: baseURL,
